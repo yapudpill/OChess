27c71763f887bcaf31f1b5fff8310c863682dc02
Marc <suga75013@gmail.com>
2024-10-10 22:54:23 +0200

    fin des fonctions de deplacements

--- bin/test_pieces.ml
+++ bin/test_pieces.ml
@@ -21,30 +21,44 @@ let from_algebrique notation =
 let sur_echiquier pos = let x,y = pos in 
   0<x && x<9 && 0<y && y<9
 
-let mouv_fou pos = []
+  let mouv_fou (x, y) =
+    filter sur_echiquier
+      (concat [init 7 (fun i -> (x + i + 1, y + i + 1));
+               init 7 (fun i -> (x + i + 1, y - i - 1));
+               init 7 (fun i -> (x - i - 1, y + i + 1));
+               init 7 (fun i -> (x - i - 1, y - i - 1))])
+  
 
-let mouv_tour pos = []
+let mouv_tour (x, y) =
+  filter sur_echiquier
+    (concat [init 7 (fun i -> (x + i + 1, y));
+             init 7 (fun i -> (x - i - 1, y));
+             init 7 (fun i -> (x, y + i + 1));
+             init 7 (fun i -> (x, y - i - 1))])
 
-let mouv_cav x y = 
+  
+
+let mouv_cav (x,y) = 
   filter sur_echiquier [(x+1,y+2);(x-1,y+2); (x-2,y+1); (x-2,y-1); (x-1,y-2); (x+1,y-2); (x+2,y-1); (x+2,y+1) ]
 
 let mouv_dame pos = (mouv_tour pos) @ (mouv_fou pos)
 
-let mouv_roi x y = 
+let mouv_roi (x,y) = 
   filter sur_echiquier [(x+1,y+1);(x,y+1); (x-1,y+1); (x-1,y); (x-1,y-1); (x,y-1); (x+1,y-1); (x+1,y) ]
 
     
-let test_piece piece x y= 
+let test_piece piece pos = 
   map to_algebrique @@ 
   (match piece with 
    | "roi" -> mouv_roi
    | "cav" -> mouv_cav
-   | _ -> mouv_roi) x y
+   | _ -> mouv_roi) pos
 
 let test_piece piece pos=
-  let x,y = from_algebrique pos in 
   map to_algebrique @@ 
   (match piece with 
    | "roi" -> mouv_roi
    | "cav" -> mouv_cav
-   | _ -> mouv_roi) x y
+   | "tour" -> mouv_tour
+   | "fou" -> mouv_fou
+   | _ -> mouv_roi) (from_algebrique pos)
