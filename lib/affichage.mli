9925877964bf0d06337292ce236b9d6eda525a82
Marc <suga75013@gmail.com>
2024-10-18 13:21:45 +0200

    #3 roque fonctionnel

--- lib/affichage.mli
+++ lib/affichage.mli
@@ -1,7 +1,8 @@
 (* Notation algébrique *)
 val to_algebrique : Piece.ptype -> int * int -> string
-val from_algebrique : string -> Piece.ptype * (int * int)
+val from_algebrique : string -> Echiquier.coup
 
 
 (* Échiquier *)
 val string_of_echiquier : ?couleur:bool -> Echiquier.t -> string
+val char_of_piece : Piece.ptype -> char
