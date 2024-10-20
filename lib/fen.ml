e4a70058ab9f3d7fb2afc134dc9ea3b418889179
Marc <suga75013@gmail.com>
2024-10-20 17:25:32 +0200

    #9 il faut donner "-" si personne ne roque

--- lib/fen.ml
+++ lib/fen.ml
@@ -2,12 +2,6 @@ open Piece
 open Echiquier
 open Partie
 
-
-
-(* Fonction pour vérifier si un caractère représente un chiffre *)
-let est_chiffre c =
-  c >= '0' && c <= '9'
-
 (* Fonction pour convertir un caractère en pièce (ptype, couleur) *)
 let piece_of_char c =
   let couleur = if Char.uppercase_ascii c = c then Blanc else Noir in
@@ -30,7 +24,7 @@ let rec from_fen_aux fen echiquier x y k =
     let c = fen.[k] in
     if c = '/' then
       from_fen_aux fen echiquier 0 (y - 1) (k + 1)  (* Sauter à la rangée suivante *)
-    else if est_chiffre c then
+    else if c >= '0' && c <= '9' then
       let avance = int_of_char c - int_of_char '0' in
       from_fen_aux fen echiquier (x + avance) y (k + 1)  (* Sauter des cases vides *)
     else (
@@ -38,17 +32,15 @@ let rec from_fen_aux fen echiquier x y k =
       from_fen_aux fen echiquier (x + 1) y (k + 1)  (* Placer la pièce et passer à la case suivante *)
     )
 
-
-
-    let rec trouver_rois echiquier x y roi_blanc roi_noir =
-      if x = 8 then (roi_blanc, roi_noir)
-      else
-        if y = 8 then trouver_rois echiquier (x + 1) 0 roi_blanc roi_noir
-        else
-          match echiquier.(x).(y) with
-          | Piece (Blanc, Roi) -> trouver_rois echiquier x (y + 1) (x, y) roi_noir  (* Continuer sur la même ligne après avoir trouvé le roi blanc *)
-          | Piece (Noir, Roi) -> trouver_rois echiquier x (y + 1) roi_blanc (x, y)  (* Continuer sur la même ligne après avoir trouvé le roi noir *)
-          | _ -> trouver_rois echiquier x (y + 1) roi_blanc roi_noir  (* Continuer à chercher *)
+let rec trouver_rois echiquier x y roi_blanc roi_noir =
+  if x = 8 then (roi_blanc, roi_noir)
+  else
+    if y = 8 then trouver_rois echiquier (x + 1) 0 roi_blanc roi_noir
+    else
+      match echiquier.(x).(y) with
+      | Piece (Blanc, Roi) -> trouver_rois echiquier x (y + 1) (x, y) roi_noir  (* Continuer sur la même ligne après avoir trouvé le roi blanc *)
+      | Piece (Noir, Roi) -> trouver_rois echiquier x (y + 1) roi_blanc (x, y)  (* Continuer sur la même ligne après avoir trouvé le roi noir *)
+      | _ -> trouver_rois echiquier x (y + 1) roi_blanc roi_noir  (* Continuer à chercher *)
 
 (* Fonction principale from_fen *)
 let from_fen (fen : string) =
@@ -61,11 +53,6 @@ let roque_of_fen roque_str =
   let roque_noir = (String.contains roque_str 'q', String.contains roque_str 'k' || String.contains roque_str 'q', String.contains roque_str 'k') in
   (roque_blanc, roque_noir)
 
-
-
-let string_couple (x,y) =
-    (string_of_int x) ^ "," ^ (string_of_int y)
-
 (* Fonction principale pour créer une partie à partir d'une FEN *)
 let creer_partie_fen (fen : string) =
   (* On découpe le FEN en différentes sections *)
@@ -73,7 +60,6 @@ let creer_partie_fen (fen : string) =
   let echiquier = from_fen @@ List.nth parts 0 in
   let roque_blanc, roque_noir = roque_of_fen @@ List.nth parts 2 in
   let roi_blanc,roi_noir = trouver_rois echiquier 0 0 (-1, -1) (-1, -1) in
-  (print_endline @@ string_couple roi_noir);
   {
     echiquier;
     trait = if List.nth parts 1 = "w" then Blanc else Noir;
