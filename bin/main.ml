c5e16f4737ea7ede5d9ba2915f4f06705486c570
Anthony Fernandes <anthony.fernandes@etu.u-paris.fr>
2024-11-02 15:21:50 +0100

    Meilleur nettoyage du terminal

--- bin/main.ml
+++ bin/main.ml
@@ -32,25 +32,25 @@ module J2 = (val Choix.choix "Veuillez sélectionner le joueur 2 :" joueurs) (R)
 
 (*** Main ***)
 let rec boucle_principale (partie, infos) =
-  print_string "\027[H\027[J"; (* Nettoie le terminal *)
+  print_string "\027[H\027[3J\027[J"; (* Nettoie le terminal *)
 
   print_echiquier ~couleur partie.echiquier;
   Printf.printf "Trait : %s\n" (string_of_couleur partie.trait);
-  Option.fold (R.string_of_infos (partie,infos) ) ~none:() ~some:(Printf.printf "Infos : %s\n");
+  Option.iter (Printf.printf "Infos : %s\n") (R.string_of_infos (partie, infos));
 
   let coup = J1.obtenir_coup (partie, infos) in
-  let partie,infos = R.jouer (partie,infos) coup in
+  let partie, infos = R.jouer (partie, infos) coup in
 
   if R.egalite (partie, infos) then (partie, None)
-  else if R.perdu (partie,infos) then (partie, Some Jeu.Piece.Blanc)
+  else if R.perdu (partie, infos) then (partie, Some Jeu.Piece.Blanc)
   else begin
     print_newline ();
     print_echiquier ~couleur partie.echiquier;
     Printf.printf "Trait : %s\n" (string_of_couleur partie.trait);
-    Option.iter (Printf.printf "Infos : %s\n") (R.string_of_infos (partie,infos));
+    Option.iter (Printf.printf "Infos : %s\n") (R.string_of_infos (partie, infos));
 
     let coup = J2.obtenir_coup (partie, infos) in
-    let partie,infos = R.jouer (partie,infos) coup in
+    let partie, infos = R.jouer (partie, infos) coup in
 
     if R.egalite (partie, infos) then (partie, None)
     else if R.perdu (partie, infos) then (partie, Some Jeu.Piece.Noir)
@@ -60,7 +60,7 @@ let rec boucle_principale (partie, infos) =
       Printf.printf "(Appuyer sur entrée pour passer au tour suivant)";
       ignore (read_line ());
 
-      boucle_principale (partie,infos)
+      boucle_principale (partie, infos)
     end
   end
 
