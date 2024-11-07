4a29aab49fbc30c7566b5f9971ddf20622a4e2c6
Anthony Fernandes <anthony.fernandes@etu.u-paris.fr>
2024-11-07 11:15:07 +0100

    Raccourcir le parsing de la notation algébrique

--- lib/entreeSortie/algebrique.ml
+++ lib/entreeSortie/algebrique.ml
@@ -20,47 +20,31 @@ let to_string =
   | Ambigu _ -> "blablabla"
 
 
-  let from_string str =
+let from_string = function
+| "o-o" | "O-O" -> Some Petit_Roque
+| "o-o-o" | "O-O-O" -> Some Grand_Roque
+| str ->
     let len = String.length str in
-    if len = 4 && str.[0] <> '@' then
-      let piece_opt = Affichage.piece_of_char (Char.uppercase_ascii str.[0]) in
-      let ambigu_info =
-        if Char.code str.[1] >= Char.code 'a' && Char.code str.[1] <= Char.code 'h' then
-          (* Ambiguïté par colonne de départ *)
-          Some (-1, String.make 1 str.[1])
-        else if Char.code str.[1] >= Char.code '1' && Char.code str.[1] <= Char.code '8' then
-          (* Ambiguïté par ligne de départ *)
-          Some (int_of_char str.[1] - 49, "")
-        else
-          None
-      in
-      match piece_opt, ambigu_info with
-      | Some p, Some (ligne_depart, col_depart) ->
-          let x = int_of_char (Char.lowercase_ascii str.[2]) - 97 in
-          let y = int_of_char str.[3] - 49 in
-          if 0 <= x && x <= 7 && 0 <= y && y <= 7 then
-            Some (Ambigu (ligne_depart, col_depart, p, (x, y)))
-          else
-            None
-      | _ -> None
+    if len < 2 || len > 4 then None
     else
-      (* Gestion des autres cas *)
-      match str with
-      | "o-o" | "O-O" -> Some Petit_Roque
-      | "o-o-o" | "O-O-O" -> Some Grand_Roque
-      | _ ->
-          if len < 2 || len > 4 || (len = 4 && str.[0] <> '@') then None
-          else
-            let placement = str.[0] = '@' in
-            let x = int_of_char (Char.lowercase_ascii str.[len - 2]) - 97 in
-            let y = int_of_char str.[len - 1] - 49 in
-            let p =
-              if len = 2 || (len = 3 && placement) then Some Jeu.Piece.Pion
-              else Affichage.piece_of_char (Char.uppercase_ascii str.[len - 3])
-            in
-            if 0 <= x && x <= 7 && 0 <= y && y <= 7 then
-              Option.map
-                (fun p -> if placement then Placement (p, (x, y)) else Arrivee (p, (x, y)))
-                p
-            else None
-
+      let placement = str.[0] = '@' in
+      let ambigu = len = 4 && not placement in
+      let x = int_of_char (Char.lowercase_ascii str.[len - 2]) - 97 in
+      let y = int_of_char str.[len - 1] - 49 in
+      let p =
+        if len = 2 || (len = 3 && placement) then Some Jeu.Piece.Pion
+        else Affichage.piece_of_char (Char.uppercase_ascii str.[if placement then 1 else 0])
+      in
+      let ambigu_info =
+        match Char.lowercase_ascii str.[1] with
+        | 'a' .. 'h' -> Some (-1, String.make 1 str.[1])
+        | '1' .. '8' -> Some (int_of_char str.[1] - 49, "")
+        | _ -> None
+      in
+      if 0 <= x && x <= 7 && 0 <= y && y <= 7 then
+        Option.bind p
+          (fun p ->
+            if placement then Some (Placement (p, (x, y)))
+            else if ambigu then Option.map (fun (l, c) -> Ambigu(l, c, p, (x, y))) ambigu_info
+            else Some (Arrivee (p, (x, y))))
+      else None
